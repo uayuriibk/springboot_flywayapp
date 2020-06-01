1b6e829df5b3543d91387917f3b82eac7b19b1e6
yuriybk <email2yurii@gmail.com>
2020-06-01 23:53:55 +0300

    flyway 1st script has been added

---
+++ src/main/resources/db/migration/V1__Create_Student_Table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `students` (
+  `studentId` int(11) NOT NULL AUTO_INCREMENT,
+  `studentName` varchar(255) DEFAULT NULL,
+  `diploma title` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`studentId`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
