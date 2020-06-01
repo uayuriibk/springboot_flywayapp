52615a9876aa7d5f84812424817345e69c6ba728
yuriybk <email2yurii@gmail.com>
2020-06-01 23:37:49 +0300

    project init

---
+++ src/main/java/com/yuriib/springboot/flywayapp/FlywayApp.java
@@ -0,0 +1,13 @@
+package com.yuriib.springboot.flywayapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlywayApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlywayApp.class, args);
+    }
+
+}
---
+++ src/test/java/com/yuriib/springboot/flywayapp/FlywayappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuriib.springboot.flywayapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlywayappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
